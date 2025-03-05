6001f24c51da30243c91a6abb9b5a17c9ab57700
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2025-03-05 11:48:38 +1100

    New Reward Function Changed 1st week of march

--- utils/train_exp_gen.py
+++ utils/train_exp_gen.py
@@ -24,7 +24,7 @@ def gen_train_exp_pool(df, pickle_save_path='exp_pool_l4s_train.pkl', train_exp_
             selected_rows = df.iloc[start_index:end_index]
             for _, selected_row in selected_rows.iterrows():
                 state = np.array(selected_row[columns_to_use], dtype=np.float32)
-                cal_reward = float((selected_row['packet_length']/ (1024 * 1034)) * 8) /  (float((selected_row['current_queue_delay']/(1000*1000)) + 0.001))
+                cal_reward = float((selected_row['packet_length']/ (1024 * 1034)) * 8) /  (float((selected_row['current_queue_delay']/(1000*1000)) + 0.00001))
                 exp_pool.add(state=state, action=selected_row['dequeue_action'], reward=cal_reward, done=0)
         
         if index > df.shape[0] * train_exp_percent:
